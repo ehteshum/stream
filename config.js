b948728f4e38c7d1399f77a0194053039b304aaa
Ehteshum <ehteshum2002@gmail.com>
2025-02-27 03:38:43 +0600

    clean

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 // Stream configuration
 const CONFIG = {
     streamUrl: "https://a.f1almostback.xyz/madrid/index.m3u8"  // Replace with your M3U8 stream URL
-}; 
-=======
-// Stream configuration
-const CONFIG = {
-    streamUrl: "https://a.f1almostback.xyz/madrid/index.m3u8"  // Replace with your M3U8 stream URL
-}; 
->>>>>>> b73f970 (ui change)
+};
