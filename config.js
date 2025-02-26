20b1a0b778c3509d6748e97872d3f402c706cbb4
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-02-27 05:22:20 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Stream configuration
 const CONFIG = {
-    streamUrl: "https://mtv.sunplex.live/MAASRANGA-TV/index.m3u8"  // Replace with your M3U8 stream URL
+    streamUrl: "https://byphdgllyk.gpcdn.net/hls/deeptotv/index.m3u8"  // Replace with your M3U8 stream URL
 };
