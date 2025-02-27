91042865f9af0705a1c08999584d42fa7e034e0a
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-02-27 12:23:11 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Stream configuration
 const CONFIG = {
-    streamUrl: "https://byphdgllyk.gpcdn.net/hls/deeptotv/index.m3u8"  // Replace with your M3U8 stream URL
+    streamUrl: "https://cdn.hoichoi24.com/LIVE/video.m3u8"  // Replace with your M3U8 stream URL
 };
