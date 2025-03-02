6c1046850b9b29ec243541faf88a1062ea0f0e44
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-03-02 15:45:30 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    streamUrl: "https://vkvsd246.okcdn.ru/hls/8206554958478.m3u8/sig/1_KtLz0U4xg/expires/1740989377044/srcIp/92.62.121.36/urls/45.136.22.56/clientType/14/srcAg/CHROME_ANDROID/mid/10145837820814/8206554958478/8206554958478_high/index.m3u8"
+    streamUrl: "https://tx2.nexgen.bz:4000//WILLOW/index.m3u8"
 };
