648a25eea402f25e6e9aae2d9dd63ba7b3da082f
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-03-05 01:59:38 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    streamUrl: "https://sonyliv.himel-op.top/index.m3u8?event-id=1090470329"
+    streamUrl: "https://sonydaimenew.akamaized.net/hls/live/2022322/DAI18ME-V3/master_mdh.m3u8?adstack=DAI-IMA&hdnea=exp=1741118386~acl=/*~id=c678ff6211da4a14afa5559ee9f565fb-1734032124505~hmac=614f725700d7ff6dbd955846becac9250fbede06b86bc8b2778539d981dd5501"
 };
