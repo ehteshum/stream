4e4f736c03942d626189a57a18c4c8121b883724
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-03-05 19:12:15 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    streamUrl: "https://sonydaimenew.akamaized.net/hls/live/2022322/DAI18ME-V3/master_mdh.m3u8?adstack=DAI-IMA&hdnea=exp=1741118386~acl=/*~id=c678ff6211da4a14afa5559ee9f565fb-1734032124505~hmac=614f725700d7ff6dbd955846becac9250fbede06b86bc8b2778539d981dd5501"
+    streamUrl: "https://d1bz14289ojrvl.cloudfront.net/out/v1/828288b4f25c407189eb6104893fad9e/index.m3u8"
 };
