91806d01408fe46525adc42273a3eed97ee01993
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-03-11 13:41:31 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    streamUrl: "http://103.163.247.39/local_out/independenttv/video.m3u8?token=At5F1MFZYnciB3"
+    streamUrl: "https://dffgtrdb.vffddd.online:443/bein3/tracks-v1a1/mono.ts.m3u8"
 };
