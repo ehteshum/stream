3368ab5c0f67540986fbddb68b0e15556ee39236
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-03-15 23:36:31 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    streamUrl: "https://director.taihotel.asia/v3/director/VE1ZTFlOGVlYmZjZGY1LWU4ZWEtMTRmNC1lZGU1LTZlN2RjOWMw/master.m3u8?md5=MrJlCKfl2kVPXI3eWjI_gw&expires=1742102119&ddg=1"
+    streamUrl: "https://rr.vipstreams.in/s/NcFYzmqOK-o4Qw8zLAaeyJWEavuoFD1ozVHJaj02fI0sUpPHOZFpUjkpsjXvxfKI/qC_ivQ2gYpcEVWYo0SoBhzvWMgrJ7MLjtdCZvIkGK_Hf6kJJVKtZ0PtOdIHr0ZPlo52raUh9ZOi9ZtUXYHK6ZA/0E3lq-YmqhQTft3yqsyCeToOpPikQ88z6T6XlAnscCIyBBmFvCWn39iy4HRq0QQF/strm.m3u8?md5=3IavxP2C5zhoSdpQruAVxQ&expiry=1742070964"
 };
