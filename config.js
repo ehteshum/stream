bcc8f3c16ac75b5480c327f86f9f6986eecea334
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-03-15 23:21:53 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    streamUrl: "https://s1.thenayeemparvez.xyz/dptv/master.m3u8?id=58015"
+    streamUrl: "https://director.taihotel.asia/v3/director/VE1ZTMyODdlZGE2MTgwLWViYmItNDczNC1kN2NjLWE2Yjg2YzQ4/master.m3u8?md5=EGqmE1OyqnzBXDg3S7tMkg&expires=1742102136&ddg=1"
 };
