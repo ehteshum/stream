52e5225edd25df30d38e61cd7c045cbec6c960a4
Ehteshum <37136242+ehteshum@users.noreply.github.com>
2025-03-24 22:52:46 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    streamUrl: "https://director.taihotel.asia/v3/director/VE1ZTMyODdlZGE2MTgwLWViYmItNDczNC1kN2NjLWE2Yjg2YzQ4/master.m3u8?md5=qy7pLnEoYNhHRRZShwdmrw&expires=1742197318&ddg=1"
+    streamUrl: "https://bldcmprod-cdn.toffeelive.com/cdn/live/sony_sports_2_hd/playlist.m3u8"
 };
